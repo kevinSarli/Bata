776d95bd4c480c4ee66d12dfc09eea52da6e253d
kevinSarli <kevin.sarli99@gmail.com>
2022-03-05 09:29:28 -0300

    NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,46 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/NavBar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+export const NavBar = () => {
+  return (
+    <>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">BATA</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#features">Productos</Nav.Link>
+              <Nav.Link href="#pricing">Favoritos</Nav.Link>
+              <NavDropdown title="Categorias" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">cat 1</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  cat 2
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  cat 3
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets">Sobre Nosotros</Nav.Link>
+              <Nav.Link eventKey={2} href="#memes">
+                usuario
+              </Nav.Link>
+              <Nav.Link eventKey={2} href="#carrito">
+                carrito
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
